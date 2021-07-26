78d30cce939adcdb0efb8791d9506807a54ee420
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-07-25 19:57:15 -0500

    TouchableOpactity, StyleSheet

--- 01-MiPrimeraApp/src/screens/ContadorScreen.tsx
+++ 01-MiPrimeraApp/src/screens/ContadorScreen.tsx
@@ -1,32 +1,41 @@
 import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 export const ContadorScreen = () => {
 
     const [contador, setContador] = useState(10);
 
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center'
-            }}
-        >
-            <Text 
-                style={{
-                    textAlign: 'center',
-                    fontSize: 40,
-                    top: -15
-                }}
-            >
-                Contador: { contador }
+        <View style={ styles.container }>
+            <Text style={ styles.title }>
+                Contador: {contador}
             </Text>
 
-            <Button 
-                title='Click'
-                onPress={ () => setContador( contador + 1) }
-            />
-
+            <TouchableOpacity
+                onPress={() => setContador(contador + 1)}
+            >
+                <View style={ styles.buttonIncrementar }>
+                    <Text>
+                        +1
+                    </Text>
+                </View>
+            </TouchableOpacity>
         </View>
-    )
-}
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 40,
+        top: -15
+    },
+    buttonIncrementar: {
+        backgroundColor: 'red', 
+        borderRadius: 100
+    }
+});

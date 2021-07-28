c8f085e1c45a52556e603bf3b3e0c615a74eb469
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-07-27 21:58:19 -0500

    Flexbox - padding, margin, border, width y heigth

--- 01-MiPrimeraApp/App.tsx
+++ 01-MiPrimeraApp/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 //import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
-import { ContadorScreen } from './src/screens/ContadorScreen';
+//import { ContadorScreen } from './src/screens/ContadorScreen';
 
 export const App = () => {
   return (
-    <ContadorScreen />
+    //<ContadorScreen />
+    <SafeAreaView style={{ flex: 1 }}>
+      <BoxObjectModelScreen />
+    </SafeAreaView>
   );
 };

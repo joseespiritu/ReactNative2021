de1d7b07bd3d42e92ae729a1d912595f16380317
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-07-28 23:16:12 -0500

    Position Relative

--- 01-MiPrimeraApp/App.tsx
+++ 01-MiPrimeraApp/App.tsx
@@ -3,13 +3,14 @@ import { SafeAreaView } from 'react-native';
 //import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 //import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 //import { ContadorScreen } from './src/screens/ContadorScreen';
-import { DimensionesScreen } from './src/screens/DimensionesScreen';
+//import { DimensionesScreen } from './src/screens/DimensionesScreen';
+import { PositionScreen } from './src/screens/PositionScreen';
 
 export const App = () => {
   return (
     //<ContadorScreen />
     <SafeAreaView style={{ flex: 1 }}>
-      <DimensionesScreen />
+      <PositionScreen />
     </SafeAreaView>
   );
 };

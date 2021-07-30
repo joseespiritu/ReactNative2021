c0e54a3794afadc2061063e154e30f35a36da63d
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-07-29 21:52:36 -0500

    Position Absolute

--- 01-MiPrimeraApp/src/screens/PositionScreen.tsx
+++ 01-MiPrimeraApp/src/screens/PositionScreen.tsx
@@ -4,6 +4,7 @@ import { View, StyleSheet } from 'react-native';
 export const PositionScreen = () => {
     return (
         <View style={styles.container}>
+            <View style={styles.cajaVerde }/>
             <View style={styles.cajaMorada }/>
             <View style={styles.cajaNaranja }/>
         </View>
@@ -13,10 +14,10 @@ export const PositionScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        //width: 300,
-        //height: 300,
-        justifyContent: 'center',
-        alignItems: 'center',
+        //width: 400,
+        //height: 400,
+        //justifyContent: 'center',
+        //alignItems: 'center',
         backgroundColor: '#28C4D9'
     },
     cajaMorada: {
@@ -24,7 +25,10 @@ const styles = StyleSheet.create({
         height: 100,
         backgroundColor: '#5856D6',
         borderWidth: 10,
-        borderColor: 'white'
+        borderColor: 'white',
+        position: 'absolute',
+        top: 0,
+        right: 0
     },
     cajaNaranja: {
         width: 100,
@@ -32,5 +36,21 @@ const styles = StyleSheet.create({
         backgroundColor: '#F0A23B',
         borderWidth: 10,
         borderColor: 'white',
+        position: 'absolute',
+        bottom: 0,
+        right: 0
+    },
+    cajaVerde: {
+        //width: 100,
+        //height: 100,
+        backgroundColor: 'green',
+        borderWidth: 10,
+        borderColor: 'white',
+        //position: 'absolute',
+        //bottom: 0,
+        //left: 0,
+        //top: 0,
+        //right: 0
+        ...StyleSheet.absoluteFillObject
     },
 });

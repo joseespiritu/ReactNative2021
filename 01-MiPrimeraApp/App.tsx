2eb09950a73ec981db6d3c9c7b8ea7366f30986f
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-07-30 21:52:11 -0500

    Flex

--- 01-MiPrimeraApp/App.tsx
+++ 01-MiPrimeraApp/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import { FlexScreen } from './src/screens/FlexScreen';
 //import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 //import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 //import { ContadorScreen } from './src/screens/ContadorScreen';
 //import { DimensionesScreen } from './src/screens/DimensionesScreen';
-import { PositionScreen } from './src/screens/PositionScreen';
+//import { PositionScreen } from './src/screens/PositionScreen';
 
 export const App = () => {
   return (
     //<ContadorScreen />
     <SafeAreaView style={{ flex: 1 }}>
-      <PositionScreen />
+      <FlexScreen />
     </SafeAreaView>
   );
 };

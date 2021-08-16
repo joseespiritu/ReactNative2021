a30962fc6bc065b720fbe8d379d6d3f9a178f6b5
joseespiritu <joseespiritu89@aragon.unam.mx>
2021-08-15 21:51:35 -0500

    Textos y primer boton

--- 02-calculadora/src/theme/appTheme.tsx
+++ 02-calculadora/src/theme/appTheme.tsx
@@ -1,13 +1,37 @@
 import { StyleSheet } from 'react-native';
 
-
 export const styles = StyleSheet.create({
     fondo: {
         flex: 1,
         backgroundColor: 'black'
     },
-    texto: {
+    calculadoraContainer: {
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent: 'flex-end'
+    },
+    resultado: {
         color: 'white',
-        fontSize: 20
+        fontSize: 60,
+        textAlign: 'right'
+    },
+    resultadoPequeno:{
+        color: 'rgba(255,255,255,0.5)',
+        fontSize: 30,
+        textAlign: 'right'
+    },
+    boton: {
+        height: 80,
+        width: 80,
+        backgroundColor: '#9B9B9B',
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+    botonTexto: {
+        textAlign: 'center',
+        padding: 10,
+        fontSize: 30,
+        color: 'black',
+        fontWeight: 'bold'
     }
 });

78be5cdf32bc05dfa04dfe647216f94df22e4bcc
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-06-04 18:21:44 -0500

    Tipado MovieDB

--- 04-PeliculasApp/src/screens/HomeScreen.tsx
+++ 04-PeliculasApp/src/screens/HomeScreen.tsx
@@ -2,19 +2,21 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import movieDB from '../api/movieDB';
 
+import { MovieDBNowPlaying } from '../interfaces/movieInterface';
+
 export const HomeScreen = () => {
 
   React.useEffect(() => {
-    movieDB.get('/now_playing')
-            .then(resp => {
-              console.log(resp);
-            })
+    movieDB.get<MovieDBNowPlaying>('/now_playing')
+      .then(resp => {
+        console.log(resp.data.results[0].title);
+      })
   }, []);
-  
+
 
   return (
     <View>
-        <Text>HomeScreen</Text>
+      <Text>HomeScreen</Text>
     </View>
   )
 }
